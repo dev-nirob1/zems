9b303a4994a471ce322bfd1cd5c7a2e090692c78
dev-nirob1 <dev.nirob1@gmail.com>
2025-06-28 10:40:10 +0600

    update statistics code

--- src/zems/Front/Components/Section/StatisticsSection.vue
+++ src/zems/Front/Components/Section/StatisticsSection.vue
@@ -4,13 +4,11 @@ import BaseParagraph from '@/components/Elements/BaseParagraph.vue'
 import BaseTitle from '@/components/Elements/BaseTitle.vue'
 import ListItem from '@/components/Elements/ListItem.vue'
 
-// Refs for each count
 const projectCount = ref(0)
 const clientCount = ref(0)
 const techStackCount = ref(0)
 const awardCount = ref(0)
 
-// Target values
 const targetValues = {
   project: 120,
   client: 80,
@@ -18,31 +16,29 @@ const targetValues = {
   award: 15,
 }
 
-// Frame-based smooth counter
-function animateCount(refValue, target, duration = 2000) {
-  const startTime = performance.now()
-
-  function update(currentTime) {
-    const elapsed = currentTime - startTime
-    const progress = Math.min(elapsed / duration, 1)
-    refValue.value = Math.floor(progress * target)
-
-    if (progress < 1) {
-      requestAnimationFrame(update)
-    } else {
-      refValue.value = target // Ensure it ends at target
-    }
-  }
-
-  requestAnimationFrame(update)
-}
-
-// Trigger on mount
 onMounted(() => {
-  animateCount(projectCount, targetValues.project)
-  animateCount(clientCount, targetValues.client)
-  animateCount(techStackCount, targetValues.techStack)
-  animateCount(awardCount, targetValues.award)
+  const interval = setInterval(() => {
+    let done = true
+
+    if (projectCount.value < targetValues.project) {
+      projectCount.value++
+      done = false
+    }
+    if (clientCount.value < targetValues.client) {
+      clientCount.value++
+      done = false
+    }
+    if (techStackCount.value < targetValues.techStack) {
+      techStackCount.value++
+      done = false
+    }
+    if (awardCount.value < targetValues.award) {
+      awardCount.value++
+      done = false
+    }
+
+    if (done) clearInterval(interval)
+  }, 20)
 })
 </script>
 
@@ -54,7 +50,7 @@ onMounted(() => {
         <ListItem>
           <i class="fa-solid fa-laptop-code fa-3x"></i>
           <div>
-            <BaseTitle>{{projectCount}}+</BaseTitle>
+            <BaseTitle>{{ projectCount }}+</BaseTitle>
             <BaseParagraph>Completed Projects</BaseParagraph>
           </div>
         </ListItem>
@@ -63,7 +59,7 @@ onMounted(() => {
         <ListItem>
           <i class="fa-solid fa-user-tie fa-3x"></i>
           <div>
-            <BaseTitle>{{clientCount}}+</BaseTitle>
+            <BaseTitle>{{ clientCount }}+</BaseTitle>
             <BaseParagraph>Clients Worldwide</BaseParagraph>
           </div>
         </ListItem>
@@ -72,7 +68,7 @@ onMounted(() => {
         <ListItem>
           <i class="fa-solid fa-layer-group fa-3x"></i>
           <div>
-            <BaseTitle>{{techStackCount}}+</BaseTitle>
+            <BaseTitle>{{ techStackCount }}+</BaseTitle>
             <BaseParagraph>Tech Stacks Used</BaseParagraph>
           </div>
         </ListItem>
@@ -112,6 +108,7 @@ onMounted(() => {
   align-items: center;
   gap: 1rem;
 }
+
 .stats-content i {
   color: var(--primary-color);
 }
